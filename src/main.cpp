111dbe75cd52ce1c71cf42c89941c519ab2437e4
its-lautaro <lautaro.lavecchia@hotmail.com>
2023-08-08 15:27:36 -0300

    remove ds1302 submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+/*
+  Simple Blink sketch
+  simple-blink.cpp
+  Use for PlatformIO demo
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  From original Arduino Blink Sketch
+  https://www.arduino.cc/en/Tutorial/Blink
+  
+  DroneBot Workshop 2021
+  https://dronebotworkshop.com
+*/
+
+// Set LED_BUILTIN if undefined or not pin 13
+// #define LED_BUILTIN 13
+
+void setup()
+{
+  // Initialize LED pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  // Set the LED HIGH
+  digitalWrite(LED_BUILTIN, HIGH);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Wait for a second
+  delay(1000);
+
+  // Set the LED LOW
+  digitalWrite(LED_BUILTIN, LOW);
+
+   // Wait for a second
+  delay(1000);
 }
